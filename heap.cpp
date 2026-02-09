9c7186a36c79c649440e7e14edfa0872fdbe5c5a
Tarushfx <tarushbajaj3@gmail.com>
2026-02-09 13:46:43 +0530

    day 14

--- heap.cpp
+++ heap.cpp
@@ -75,3 +75,83 @@ public:
         return mnpq.top();
     }
 };
+
+// k most frequent elements
+#define pii pair<int, int>
+class Compare
+{
+public:
+    bool operator()(pii A, pii B)
+    {
+        return A.first >= B.first;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        map<int, int> mp;
+        for (auto x : nums)
+            mp[x]++;
+        priority_queue<pii, vector<pii>, Compare> pq;
+        for (auto [x, freq] : mp)
+        {
+            pq.push({freq, x});
+            if (pq.size() > k)
+                pq.pop();
+        }
+        vector<int> ans;
+        while (!pq.empty())
+        {
+            auto [f, x] = pq.top();
+            pq.pop();
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
+
+// k  closest points to origin
+#define pii pair<int, int>
+int square(int a)
+{
+    return a * a;
+}
+int dist(pii a)
+{
+    return square(a.first) + square(a.second);
+}
+class Compare
+{
+public:
+    bool operator()(pii A, pii B)
+    {
+        return dist(A) <= dist(B);
+    }
+};
+class Solution
+{
+public:
+    vector<vector<int>> kClosest(vector<vector<int>> &points, int k)
+    {
+        priority_queue<pii> pq;
+        for (int i = 0; i < points.size(); i++)
+        {
+            auto point = points[i];
+            pq.push(
+                {dist({point[0], point[1]}), i});
+            if (pq.size() > k)
+                pq.pop();
+        }
+        vector<vector<int>> ans;
+        while (!pq.empty())
+        {
+            auto [d, i] = pq.top();
+            ans.push_back(points[i]);
+            pq.pop();
+        }
+        return ans;
+    }
+};
