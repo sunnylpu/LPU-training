2d1571b69660cd7a5fecc7fd775ef5aabce128af
Tarushfx <tarushbajaj3@gmail.com>
2026-02-18 12:32:25 +0530

    day 21 part 2

--- graph.cpp
+++ graph.cpp
@@ -44,3 +44,42 @@ public:
         return cnt;
     }
 };
+// max area
+class Solution
+{
+public:
+    int dfs(vector<vector<int>> &grid, int r, int c, vector<vector<bool>> &visited)
+    {
+        int n = grid.size(), m = grid[0].size();
+        if (r >= n || r < 0 || c >= m || c < 0)
+            return 0;
+        if (visited[r][c])
+            return 0;
+        if (grid[r][c] == 0)
+            return 0;
+        visited[r][c] = true;
+        int area = 1;
+        area += dfs(grid, r + 1, c, visited);
+        area += dfs(grid, r, c + 1, visited);
+        area += dfs(grid, r, c - 1, visited);
+        area += dfs(grid, r - 1, c, visited);
+        return area;
+    }
+    int maxAreaOfIsland(vector<vector<int>> &grid)
+    {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        int cnt = 0;
+        for (int r = 0; r < n; r++)
+        {
+            for (int c = 0; c < m; c++)
+            {
+                if (grid[r][c] == 0 || visited[r][c])
+                    continue;
+                int area = dfs(grid, r, c, visited);
+                cnt = max(cnt, area);
+            }
+        }
+        return cnt;
+    }
+};
